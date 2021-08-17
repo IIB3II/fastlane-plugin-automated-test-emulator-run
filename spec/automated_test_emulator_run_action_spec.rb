df6296c5d071592c8037c88d51853287ebf82bcd
IIB3II <aa4a@inbox.lv>
2021-08-17 18:08:24 +0300

    Update automated_test_emulator_run_action_spec.rb

--- spec/automated_test_emulator_run_action_spec.rb
+++ spec/automated_test_emulator_run_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::AutomatedTestEmulatorRunAction do
   describe '#run' do
     it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The automated_test_emulator_run plugin is working!")
+      expect(Fastlane::UI).to receive(:message).with("The automated_test_emulator_run plugin FORK for Intexsys is working!")
 
       Fastlane::Actions::AutomatedTestEmulatorRunAction.run(nil)
     end
